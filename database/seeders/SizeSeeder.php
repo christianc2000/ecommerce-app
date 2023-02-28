64dfaabeef9802220944da0503ed392db87ff1c8
christianc2000 <christiancelsomamanisoliz0@gmail.com>
2023-02-28 16:13:56 -0400

    primer Commit

---
+++ database/seeders/SizeSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use App\Models\Size;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Seeder;
+
+class SizeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $products = Product::whereHas('subcategory', function (Builder $query) {
+            $query->where('color', true)->where('size', true);
+        })->get();
+
+        $sizes = ['Talla S', 'Talla M', 'Talla L'];
+        foreach ($products as $product) {
+            foreach ($sizes as $size) {
+                Size::create([
+                    'name' => $size,
+                    'product_id' => $product->id
+                ]);
+            }
+        }
+    }
+}
